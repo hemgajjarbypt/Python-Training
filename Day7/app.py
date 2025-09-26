b63d24584b61c128c5309ebcc8e0984c7d9cdf36
Hem Gajjar <hem.gajjar@bypeopletechnologies.com>
2025-09-26 17:19:01 +0530

    Add Day 7.

---
+++ Day7/app.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from database import SessionLocal, Book as BookModel, Base, engine
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+class Book(BaseModel):
+    title: str
+    author: str
+    year: int
+    
+# Dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# create book route
+@app.post("/books")
+async def create_book_api(book: Book, db: Session = Depends(get_db)):
+
+    db_book = BookModel(title=book.title, author=book.author, year=book.year)
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    
+    return {"message": "Book added successfully", "task": {"title": db_book.title}}
+
+# Get all books
+@app.get("/books")
+async def get_all_books(db: Session = Depends(get_db)):
+    books = db.query(BookModel).all()
+    return {"books": [{"id": b.id, "title": b.title, "author": b.author, "year": b.year} for b in books]}
+
+# Delete books by it
+@app.delete("/books/{id}")
+async def delete_book(id: int, db: Session = Depends(get_db)):
+    existing_book = db.query(BookModel).filter(BookModel.id == id).first()
+    if not existing_book:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    db.delete(existing_book)
+    db.commit()
+
+    return {"message": f"Book with id {id} deleted successfully"}
--- Day7/database.py
+++ Day7/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./tasks.db"
+DATABASE_URL = "sqlite:///./books.db"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
@@ -12,8 +12,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 # User table
-class Task(Base):
-    __tablename__ = "tasks"
+class Book(Base):
+    __tablename__ = "books"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True, unique=True)
+    author = Column(String)
+    year = Column(Integer)
